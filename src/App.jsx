48362c1cbf09a7b4bbd5b1d7721494ac01e50266
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-08-02 18:39:22 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,19 @@
 import "./assets/styles/generic-styles.scss";
-import Sort from "./components/Sort/Sort";
-import Search from "./components/Search/Search";
-import Results from "./components/Results/Results";
-import Paginator from "./components/Paginator/Paginator";
-import { useState, useEffect } from "react";
+import Explorer from "./routes/Explorer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [sortQuery, setSortQuery] = useState(undefined);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [results, setResults] = useState([]);
-  const [totalItems, setTotalItems] = useState(0);
-
-  useEffect(() => {
-    getSearchResult();
-  }, [searchQuery, sortQuery, pageNumber]);
-
-  function setAPIText(query) {
-    setSearchQuery(query);
-  }
-
-  function setAPISortOrder(newSort) {
-    setSortQuery(newSort);
-  }
-
-  function setPageQuery(number) {
-    setPageNumber(number);
-  }
-
-  function getSearchResult() {
-    if (searchQuery) {
-      const endpoint = "https://api.github.com/search/repositories";
-      const pagination = "&per_page=10" + "&page=" + pageNumber;
-      const sortQueries = sortQuery
-        ? `&sort=${sortQuery.element}&order=${sortQuery.order}`
-        : "";
-      const endpointWithQueries =
-        endpoint + "?q=" + searchQuery + pagination + sortQueries;
-
-      fetch(endpointWithQueries)
-        .then((res) => res.json())
-        .then((data) => {
-          setResults(data.items);
-          setTotalItems(data.total_count);
-        });
-    }
-  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Explorer />,
+    },
+  ]);
 
   return (
     <>
       (
-      <div className="page">
-        <h1 className="page-title">GitHub Explorer</h1>
-        <div className="explorer">
-          <Search setAPIText={setAPIText}></Search>
-          <Sort setAPISortOrder={setAPISortOrder}></Sort>
-          <Results resultsArray={results}></Results>
-          <Paginator
-            totalItems={totalItems}
-            setPageNumber={setPageQuery}
-          ></Paginator>
-        </div>
-      </div>
-      )
+      <RouterProvider router={router} />)
     </>
   );
 }
